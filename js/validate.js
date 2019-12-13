07a9fc46fa434aeeffe4b467976ef1d4f14376f9
mediagymnasiet-webbutveckling <55544791+mediagymnasiet-webbutveckling@users.noreply.github.com>
2019-12-13 08:29:30 +0100

    first commit

---
+++ js/validate.js
@@ -0,0 +1,22 @@
+
+/* HÄR SKRIVER DU DIN KOD SOM SKA VALIDERA FORMULÄRET
+ * 
+ * Scriptet fungerar enliget nuvarande startkod:
+ * Om funktionen validateForm() returnar true skickas användaren till dummy.html.
+ * Om funktionen returnerar false stannar användaren kvar på index.html 
+ * 
+ * Obs! För att lösa uppgiften behövs du endast behöva skriva js-kod i denna fil. 
+ * 
+ * Du är fri att skriva fler funktioner i denna fil. Du bestämmer själv hur du strukturer de funktioner och de variabler du behöver.
+ * 
+ */
+
+
+function validateForm() {
+
+    
+return false;
+}
+
+
+
